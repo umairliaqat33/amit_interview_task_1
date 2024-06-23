53c59776ec886171af82a3e701674b5b23c9997c
umairliaqat33 <umairliaqat552@gmail.com>
2024-06-23 11:05:31 +0500

    first commit

---
+++ lib/utils/collection_names.dart
@@ -0,0 +1,18 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+
+// class CollectionsNames {
+//   static final firestoreCollection = FirebaseFirestore.instance;
+//   static final firebaseAuth = FirebaseAuth.instance;
+//   static String usersCollection = 'users';
+//   static String driverCollection = 'drivers';
+//   static String patientCollection = 'patients';
+//   static String hospitalCollection = 'hospitals';
+//   static String doctorCollection = 'doctors';
+//   static String usedID = 'usedId';
+//   static String requestInProgressCollection = 'requestInProgress';
+//   static String completedRequestCollection = 'completedRequest';
+//   static String bedsCollection = 'beds';
+//   static String reportsCollection = 'reports';
+//   static String notificationCollection = 'notifications';
+// }
